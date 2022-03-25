cb5b225f15f75004f28d1417016c4ee9fae966f2
maisha <maisha@askeddi.com>
2022-03-25 20:41:00 +0000

    fix: converted the app to androidX as the packages was not running

--- app/src/main/java/com/hope/cs/fooddeliveryapp/SplashScreenActivity.java
+++ app/src/main/java/com/hope/cs/fooddeliveryapp/SplashScreenActivity.java
@@ -1,7 +1,13 @@
 package com.hope.cs.fooddeliveryapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Handler;
+//import android.support.v7.app.ActionBar;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -9,5 +15,19 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+//        Hiding the title from the splash screen
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+//        Making the splash screen to be visible for 2 seconds and redirecting it to the mainActivity screen
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this,MainActivity.class));
+                finish();
+            }
+        },2000);
     }
 }
