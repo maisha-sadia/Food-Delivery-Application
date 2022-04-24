fc9c51795b4ba0402a84194e5ec615e7c75d977f
maisha <maisha@askeddi.com>
2022-04-24 15:50:26 +0100

    feat: Added animation to the splash screen

--- app/src/main/java/com/hope/cs/fooddeliveryapp/SplashScreenActivity.java
+++ app/src/main/java/com/hope/cs/fooddeliveryapp/SplashScreenActivity.java
@@ -1,27 +1,45 @@
 package com.hope.cs.fooddeliveryapp;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
 import android.os.Handler;
 //import android.support.v7.app.ActionBar;
 //import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
+    ImageView imageView;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
 //        Hiding the title from the splash screen
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+        imageView = (ImageView)findViewById(R.id.starting_logo);
+        textView = (TextView)findViewById(R.id.ownerName);
+
+        imageView.animate().alpha(0f).setDuration(0);
+        textView.animate().alpha(0f).setDuration(0);
+
+        imageView.animate().alpha(1f).setDuration(1000).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                textView.animate().alpha(1f).setDuration(800);
+            }
+        });
+
 
 //        Making the splash screen to be visible for 2 seconds and redirecting it to the mainActivity screen
-
+//
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
